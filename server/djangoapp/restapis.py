c8403bfe81f1895a2a6db3368762413aeae26df6
Ryan <168155235+ikar888@users.noreply.github.com>
2025-04-04 18:19:06 +0800

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -6,6 +6,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.ERROR)
+
 backend_url = os.getenv(
     'backend_url', default="http://localhost:3030")
 sentiment_analyzer_url = os.getenv(
